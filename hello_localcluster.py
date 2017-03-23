fdb7284a706978efbcb359c4dfd7152c607d5d33
Matyas Selmeci <matyas@cs.wisc.edu>
2017-03-23 10:50:51 -0500

    initial

---
+++ hello_localcluster.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import tornado
+import distributed
+import time
+
+def sleepsort(arg):
+    time.sleep(arg/10.0)
+    print(arg)
+    return arg
+
+cluster = distributed.LocalCluster()
+client = distributed.Client(cluster)
+A = client.map(ord, 'HELLO')
+B = client.map(sleepsort, A, pure=False)
+C = client.map(chr, B)
+print(client.gather(C))
+try:
+    client.shutdown()
+except distributed.core.CommClosedError:
+    pass
+
