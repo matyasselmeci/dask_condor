6b0fc9244fd23457ba224c72a581caab9e1f946b
Matyas Selmeci <matyas@cs.wisc.edu>
2017-05-22 16:15:10 -0500

    Bump up data size

--- experiments/weather/downloaddata.py
+++ experiments/weather/downloaddata.py
@@ -5,8 +5,7 @@ import requests
 #sites = ['MSN', 'MKE', 'GRB', 'EAU', 'LSE', 'LNR', 'VOK', 'JVL', 'MTW', 'CWA', 'OSH', 'RHI', 'CMY', 'AUW']
 sites = ['MSN']
 year_start = 2007
-#year_end = 2016
-year_end = 2007
+year_end = 2016
 url = 'http://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?station={0}&data=tmpc&data=dwpc&data=relh&data=drct&data=sknt&data=mslp&data=p01m&year1={1}&month1=1&day1=1&year2={2}&month2=12&day2=31&tz=Etc%2FUTC&format=comma&latlon=no&direct=no&report_type=1&report_type=2'
 csvfile = "{0}-{1}-to-{2}.csv"
 # format with (site, year_start, year_end)
