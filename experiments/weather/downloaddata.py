67f5d3f8cac4f34690f2ceda7420f92e83482638
Matyas Selmeci <matyas@cs.wisc.edu>
2017-05-22 16:29:49 -0500

    More data! Also print timings and use requests better

--- experiments/weather/downloaddata.py
+++ experiments/weather/downloaddata.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python
 
 import requests
+import time
 
-#sites = ['MSN', 'MKE', 'GRB', 'EAU', 'LSE', 'LNR', 'VOK', 'JVL', 'MTW', 'CWA', 'OSH', 'RHI', 'CMY', 'AUW']
-sites = ['MSN']
+def download(site, year_start, year_end):
+    url = "http://mesonet.agron.iastate.edu/cgi-bin/request/asos.py"
+    params = { 'station': site
+             , 'data': ['tmpc', 'dwpc', 'relh', 'drct', 'sknt', 'mspl', 'p01m']
+             , 'year1': year_start
+             , 'month1': 1
+             , 'day1': 1
+             , 'year2': year_end
+             , 'month2': 12
+             , 'day2': 31
+             , 'tz': 'Etc/UTC'
+             , 'format': 'comma'
+             , 'latlon': 'no'
+             , 'report_type': [1, 2]
+    }
+
+    return requests.get(url, params)
+
+sites = ['MSN', 'MKE', 'GRB', 'EAU', 'LSE', 'LNR', 'VOK', 'JVL', 'MTW', 'CWA', 'OSH', 'RHI', 'CMY', 'AUW']
 year_start = 2007
 year_end = 2016
-url = 'http://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?station={0}&data=tmpc&data=dwpc&data=relh&data=drct&data=sknt&data=mslp&data=p01m&year1={1}&month1=1&day1=1&year2={2}&month2=12&day2=31&tz=Etc%2FUTC&format=comma&latlon=no&direct=no&report_type=1&report_type=2'
 csvfile = "{0}-{1}-to-{2}.csv"
 # format with (site, year_start, year_end)
 
 for site in sites:
-    r = requests.get(url.format(site, year_start, year_end))
+    r = download(site, year_start, year_end)
     with open(csvfile.format(site, year_start, year_end), 'wb') as outfile:
         for chunk in r.iter_content(chunk_size=128):
             outfile.write(chunk)
-
+    time.sleep(5)
