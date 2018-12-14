9186573958a7c22824ec47d29df5d7cc009171ed
Matyas Selmeci <matyas@cs.wisc.edu>
2018-12-14 13:30:11 -0600

    Removed files that were moved to matyasselmeci/dask_condor_experiments; add MOVED file

--- dmagellan/pythonenv.sh
+++
@@ -1,2 +0,0 @@
-export PATH=~/python-dmagellan/bin/:$PATH
-export PYTHONPATH=~/dask_condor:~/python-dmagellan/lib/python2.7/site-packages:/usr/lib64/python2.6/site-packages
--- piltest/pythonenv.sh
+++
@@ -1,2 +0,0 @@
-export PATH=~/python/bin:$PATH
-export PYTHONPATH=~/dask_condor:~/python/lib/python2.7/site-packages:/usr/lib64/python2.6/site-packages
