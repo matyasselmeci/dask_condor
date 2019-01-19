6b03cc7fb53f84b25b5d0464b3c1f24ec290d5ce
Matyas Selmeci <matyas@cs.wisc.edu>
2019-01-18 18:47:58 -0600

    Add requirements.txt; use it in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import setup
 
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
 setup(
     name='dask_condor',
     version='0.0.20181214',
@@ -10,10 +13,6 @@ setup(
     author_email='matyas@cs.wisc.edu',
     url='https://github.com/matyasselmeci/dask_condor',
     packages=['dask_condor'],
-    install_requires=['dask',
-                      'distributed',
-                      'htcondor>=8.6',
-                      'tornado>=5.0',
-                      ],
+    install_requires=install_requires,
     license="Apache Software License",
 )
